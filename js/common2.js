6ccf3a897cd8dcaa45ac4b3f3e437178aa5ffa86
vidhigavadia <vidhi.gavadia@gmail.com>
2016-02-22 15:22:08 -0600

    Update common2.js

--- js/common2.js
+++ js/common2.js
@@ -9,7 +9,7 @@ function carMakeModelLoad(passID) {
 	else{
 		$("#Model").append("<option value="option6">Test</option>");
 	}
-		/*else {
+	/*	else {
 		if (passID == "not") {
 			myData = {'fnName':'modelFromMake' ,'carMake': makeValue};
 		} else {
@@ -33,8 +33,8 @@ function carMakeModelLoad(passID) {
 				$("#Model").empty();
 				$("#Model").append($("<option />").val('').text(''));
 			}
-   		}); */
-	}
+   		}); 
+	} */
 }
 
 
