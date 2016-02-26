0b8921a1b3e3965b5878cfef31d3c6d302a261f7
vidhigavadia <vidhi.gavadia@gmail.com>
2016-02-26 14:59:58 -0600

    Update config.php

--- config.php
+++ config.php
@@ -42,13 +42,13 @@ $instance_url = $response['instance_url'];
 
 $formErrors = array();
 $post_data = $_POST;
-$form_data = json_decode($post_data['data_json']);
-error_log("response: $post_data");
+$form_data = json_decode($post_data['data_json'],true);
+error_log("response: $post_data['data_json']");
 		//if (firstnameValid($_POST['First_Name'])) $GLOBALS['formErrors'][] = firstnameValid($_POST['First_Name']);
 	//	if (lastnameValid($_POST['Last_Name'])) $GLOBALS['formErrors'][] = lastnameValid($_POST['Last_Name']) ;
 	if(isset($_POST['First_Name'])) echo "<p>present</p>";
 	if(isset($_POST['Last_Name'])) echo "<p>present2</p>";
-	if($form_data->last_name[0]!='') echo "<p>json last</p>";
+	if($form_data['last_name'][0]!='') echo "<p>json last</p>";
 		
 //	if(sizeof($GLOBALS['formErrors']) == 0) {
 		include_once 'lead.php';
@@ -58,7 +58,7 @@ error_log("response: $post_data");
 	//	if(isset($_POST['Last_Name']) && $_POST['Last_Name'] != '') $formValues['LastName'] = $_POST['Last_Name'];
 		
 		
-		if($form_data->last_name[0]!='') $formValues['LastName'] = $form_data->last_name[0];
+		if($form_data['last_name'][0]!='') $formValues['LastName'] = $form_data['last_name'][0];
 	//	if(isset($_POST['Last_Name']) && $_POST['Last_Name'] != '') $formValues['LastName'] = $_POST['Last_Name'];
 		
 		$formValues['RecordTypeid'] = '012320000009eyu'; 
