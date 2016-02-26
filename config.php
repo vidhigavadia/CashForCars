286a149d5ee3afc7a6b0dd089006c4bc74b421d3
vidhigavadia <vidhi.gavadia@gmail.com>
2016-02-26 14:54:15 -0600

    Update config.php

--- config.php
+++ config.php
@@ -43,6 +43,7 @@ $instance_url = $response['instance_url'];
 $formErrors = array();
 $post_data = $_POST;
 $form_data = json_decode($post_data['data_json']);
+error_log("response: $post_data");
 		//if (firstnameValid($_POST['First_Name'])) $GLOBALS['formErrors'][] = firstnameValid($_POST['First_Name']);
 	//	if (lastnameValid($_POST['Last_Name'])) $GLOBALS['formErrors'][] = lastnameValid($_POST['Last_Name']) ;
 	if(isset($_POST['First_Name'])) echo "<p>present</p>";
