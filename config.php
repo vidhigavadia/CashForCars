8590586dfc1c882d0ac569795779bf6e062b6dcc
vidhigavadia <vidhi.gavadia@gmail.com>
2016-03-01 16:40:31 -0600

    Update config.php

--- config.php
+++ config.php
@@ -6,8 +6,7 @@ include_once 'form_validate.php';
 $formErrors = array();
 $post_data = $_POST['data_json'];
 $form_data = json_decode($post_data,true);
-//error_log( print_R($post_data,TRUE) );
-error_log($_POST);
+error_log(print_R($_POST,TRUE) );
 error_log($post_data);
 	
 
