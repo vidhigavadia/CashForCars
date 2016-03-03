6b3fa4d4b60c8601d2e6e73f18da8af5e7f864f4
vidhigavadia <vidhi.gavadia@gmail.com>
2016-03-03 17:35:11 -0600

    Update data_connection.php

--- data_connection.php
+++ data_connection.php
@@ -12,12 +12,13 @@ $dsn = 'pgsql:'
     . 'port=' . $dbopts["port"].';'
     . 'sslmode=require;'
     . 'password='. $dbopts["pass"];
-function getSFCredentials() {
+    
+function getSFData() {
 
 try
 {
 	$db = new PDO($dsn);
-	$query = "select username , convert_from(decrypt(password,'d0a7e7997b6d5fcd55f4b5c32611b87cd923e88837b63bf2941ef819dc8ca282','aes'),'utf-8') as pass from credentials;";
+	$query ="select convert_from(decrypt(username,'iu8758SB*&%EGE$)kjkb7666klS472.?/:2Fr4DFJ2324saf&*(*%#@!^','aes'),'utf-8') as username , convert_from(decrypt(password,'iu8758SB*&%EGE$)kjkb7666klS472.?/:2Fr4DFJ2324saf&*(*%#@!^','aes'),'utf-8') as password, convert_from(decrypt(id,'iu8758SB*&%EGE$)kjkb7666klS472.?/:2Fr4DFJ2324saf&*(*%#@!^','aes'),'utf-8') as client_id , convert_from(decrypt(secret,'iu8758SB*&%EGE$)kjkb7666klS472.?/:2Fr4DFJ2324saf&*(*%#@!^','aes'),'utf-8') as client_secret from sf_data;"; 
 $result = $db->query($query);
 while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
 //	$credentials[0]= $row['username'];
@@ -37,27 +38,6 @@ catch(PDOException $pe)
 }
 
 
-function getSFData() {
-
-try
-{
-	$db = new PDO($dsn);
-	$query = "select convert_from(decrypt(id,'iu8758SB*&%EGE$)kjkb7666klS472.?/:2Fr4DFJ2324saf&*(*%#@!^','aes'),'utf-8') as client_id , convert_from(decrypt(secret,'iu8758SB*&%EGE$)kjkb7666klS472.?/:2Fr4DFJ2324saf&*(*%#@!^','aes'),'utf-8') as client_secret from client_values;";
-  $result = $db->query($query);
-while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-return $row;
-}
-$result->closeCursor();
-
-	
-}
-catch(PDOException $pe)
-{
-	die('Connection error, because: ' .$pe->getMessage());
-}
- 
-
-}
 
 
 ?>
