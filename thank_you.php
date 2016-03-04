b8cdbe436a8b2dcc51998b55d3895c0da71c64b0
vidhigavadia <vidhi.gavadia@gmail.com>
2016-03-04 10:55:39 -0600

    Create thank_you.php

---
+++ thank_you.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+<title>Thank You</title>
+<link rel="stylesheet" href="/library/css/donationForm2.css" />
+
+<style type="text/css">
+	.anotherBtn {padding: 5px; font-family:Arial, Helvetica, sans-serif;font-size: 14px;}
+	a {text-decoration: none; color: white; background-color: #b4b4b4; padding: 5px;-moz-border-radius: 7px; border-radius: 7px;}
+	a:hover {background-color: #919191; } 
+</style>
+
+</head>
+
+<body>
+<?php //include_once("form_thankyou_ga.php"); session_start(); 
+?> 
+<div id="thankyou">
+	<table style="border: 5px solid #CC0000;-moz-border-radius: 3px; border-radius: 3px;">
+		<tr><td class="tuheader">THANK YOU</td></tr>
+		<tr><td class="anotherBtn">Thank you for your generous donation. We will call you within 24 hours to confirm and complete the donation process. You can also call us at 800-477-1335 with any questions or concerns.</td></tr>
+		<tr><td class="anotherBtn" style="text-align: center;">
+		<a href="http://sell.copartdirect.com/testing_it">Donate Another Vehicle</a></td></tr>
+		<tr><td></td></tr>
+	</table>
+</div>
+
+</body>
