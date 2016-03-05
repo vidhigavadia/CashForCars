1f596255742369748c04e830931ed51dfc0eef74
vidhigavadia <vidhi.gavadia@gmail.com>
2016-03-04 18:14:26 -0600

    Update data_connection.php

--- data_connection.php
+++ data_connection.php
@@ -36,6 +36,76 @@ catch(PDOException $pe)
 }
 
 
+function insertAccessData($access,$url) {
+require_once 'vendor/autoload.php';
+$app = new Silex\Application();
+$app['debug'] = true;
+$dbopts = parse_url(getenv('DATABASE_URL'));
+
+$dsn = 'pgsql:'
+    . 'host='.$dbopts["host"]. ';'
+    . 'dbname='.ltrim($dbopts["path"],'/').';'
+    . 'user='.$dbopts["user"].';'
+    . 'port=' . $dbopts["port"].';'
+    . 'sslmode=require;'
+    . 'password='. $dbopts["pass"];
+try
+{
+	$db = new PDO($dsn);
+	$query ="insert into access values($access,$url)";
+ $result = pg_query($query); 
+        if (!$result) { 
+            $errormessage = pg_last_error(); 
+            echo "Error with query: " . $errormessage; 
+            exit(); 
+        } 
+
+pg_close(); 
+//var_dump($credentials);
+	
+}
+catch(PDOException $pe)
+{
+	die('Connection error, because: ' .$pe->getMessage());
+}
+ 
+
+}
+
+function getAccessData() {
+require_once 'vendor/autoload.php';
+$app = new Silex\Application();
+$app['debug'] = true;
+$dbopts = parse_url(getenv('DATABASE_URL'));
+
+$dsn = 'pgsql:'
+    . 'host='.$dbopts["host"]. ';'
+    . 'dbname='.ltrim($dbopts["path"],'/').';'
+    . 'user='.$dbopts["user"].';'
+    . 'port=' . $dbopts["port"].';'
+    . 'sslmode=require;'
+    . 'password='. $dbopts["pass"];
+try
+{
+	$db = new PDO($dsn);
+	$query ="select * from access";
+$result = $db->query($query);
+while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+//	$credentials[0]= $row['username'];
+	//$credentials[1] =$row['pass'];
+	return $row;
+}
+$result->closeCursor();
+//var_dump($credentials);
+	
+}
+catch(PDOException $pe)
+{
+	die('Connection error, because: ' .$pe->getMessage());
+}
+ 
+
+}
 
 
 ?>
