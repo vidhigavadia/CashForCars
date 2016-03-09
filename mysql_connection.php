2bfdcf1e624bf677fd73189e41f0fb14f8f65097
vidhigavadia <vidhi.gavadia@gmail.com>
2016-03-09 10:51:15 -0600

    Update mysql_connection.php

--- mysql_connection.php
+++ mysql_connection.php
@@ -8,7 +8,7 @@ $db = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
 if ($db === false){
 		error_log("Common.php: MYSQL connect error: " . mysqli_connect_error());
 }
-
+ $key = '!@VWfekj5bwe^4db@M0pAkDk;ccnwd!``~][asdBm;<p';
 /*try {
 	global $db;
 	$host = DB_HOST;  $dbname = DB_DATABASE;
@@ -21,7 +21,7 @@ function getSFData() {
 
 
 
-	$query ="select convert_from(decrypt(username,'iu8758SB*&%EGE$)kjkb7666klS472.?/:2Fr4DFJ2324saf&*(*%#@!^','aes'),'utf-8') as username , convert_from(decrypt(password,'iu8758SB*&%EGE$)kjkb7666klS472.?/:2Fr4DFJ2324saf&*(*%#@!^','aes'),'utf-8') as password, convert_from(decrypt(id,'iu8758SB*&%EGE$)kjkb7666klS472.?/:2Fr4DFJ2324saf&*(*%#@!^','aes'),'utf-8') as client_id , convert_from(decrypt(secret,'iu8758SB*&%EGE$)kjkb7666klS472.?/:2Fr4DFJ2324saf&*(*%#@!^','aes'),'utf-8') as client_secret from sf_data;"; 
+$query ="select CONVERT(AES_DECRYPT(username,'$key') using utf8) as username , CONVERT(AES_DECRYPT(password,'$key') using utf8) as password, CONVERT(AES_DECRYPT(id,'$key') using utf8) as client_id , CONVERT(AES_DECRYPT(secret,'$key') using utf8) as client_secret from sf_data;"; 
 $result = $db->query($query);
 while ($row = $result->fetch_assoc()) {
 //	$credentials[0]= $row['username'];
@@ -38,7 +38,7 @@ function insertAccessData($access,$url) {
 
 	$time = date('Y-m-d H:i:s');
 	
-	 $stmt = "INSERT INTO access VALUES ('$access','$url','$time')";
+	 $stmt = "INSERT INTO access VALUES (AES_ENCRYPT('$access','$key'),AES_ENCRYPT('$url','$key'),AES_ENCRYPT('$time','$key'))";
 	 $retval = mysql_query( $stmt, $db );
   if($retval) {
       echo '1 row has been inserted';  
@@ -63,7 +63,7 @@ function updateAccessData($access,$new_access) {
 
 	$time = date('Y-m-d H:i:s');
 	
-	 $stmt = "UPDATE access SET token='$new_access' where token='$access'";
+	 $stmt = "UPDATE access SET token=AES_ENCRYPT('$new_access','$key') where token=AES_ENCRYPT('$access','$key')";
 	
   if ($db->query($stmt) === TRUE) {
     echo "Record updated successfully";
