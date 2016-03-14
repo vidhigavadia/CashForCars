328cb92a956f0151c2d88d39d1f5e05d4ae25840
vidhigavadia <vidhi.gavadia@gmail.com>
2016-03-14 14:37:58 -0500

    Update common2.js

--- js/common2.js
+++ js/common2.js
@@ -62,6 +62,10 @@ function hideSubmit() {
 	var s = document.getElementById( 'registerButton' );
 	s.style.opacity = '0.7';
 	s.disabled=true;
+	
+	var ss = document.getElementById( 'registersButton' );
+	ss.style.opacity = '0.7';
+	ss.disabled=true;
 }
 
 //Function to show Donate my Vehicle button
@@ -69,6 +73,11 @@ function showsubmit() {
 var s = document.getElementById( 'registerButton' );
     s.style.opacity = '1';
     s.disabled=false;
+    
+    
+    var ss = document.getElementById( 'registersButton' );
+    ss.style.opacity = '1';
+    ss.disabled=false;
 }
 
 function reduceClick() {
@@ -887,6 +896,94 @@ $('#workphoneb').keyup(function() {
 
 //Function to validate each mandatory missing fields while doing a form submission
 $('#registerButton').click(function(e) {
+	var fname = $.trim($('#First_Name').val());
+	var lname = $.trim($('#Last_Name').val());
+	var email = $.trim($('#Email').val());
+/*	var addr1 = $.trim($('#Mailing_Address1').val());
+	var city = $.trim($('#City').val());
+	var state = $.trim($('#State :selected').text()); */
+	var zip = $.trim($('#Zip').val());
+	var homephone1 = $.trim($('#Home_Telephone_Ac').val());
+	var homephone2 = $.trim($('#homephonea').val());
+	var homephone3 = $.trim($('#homephoneb').val());
+
+	// First name 
+	if(fname==='') {
+	e.preventDefault();
+    $('#fnamefail').show();
+    $('#fnamefail').text('First Name is required');
+    $("#fnamefail").css("color","red");
+	} else { fnameTest(e); }
+	
+// Last name
+	if(lname==='') {
+		e.preventDefault();
+    $('#lnamefail').show();
+    $('#lnamefail').text('Last Name is required');
+    $("#lnamefail").css("color","red");
+	} else { lnameTest(e); }
+
+	emailTest(e);
+	
+	// Address Line-1
+/*	if(addr1==='') {
+		e.preventDefault();
+  $('#adr1fail').show();
+    $('#adr1fail').text('Address Line one is required');
+    $("#adr1fail").css("color","red");
+	} else { address1Test(e); }
+	
+	address2Test(e);
+	
+	// City
+	if(city==='') {
+		e.preventDefault();
+    $('#cityfail').show();
+    $('#cityfail').text('City is required');
+    $("#cityfail").css("color","red");
+	} else { cityTest(e); }
+	
+	// State
+	if(state==='') {
+		e.preventDefault();
+    $('#statefail').show();
+    $('#statefail').text('State must be selected');
+    $("#statefail").css("color","red"); 
+	} */
+	
+	// Zipcode
+	if(zip==='') {
+		e.preventDefault();
+    $('#zipfail').show();
+    $('#zipfail').text('Zipcode is required');
+    $("#zipfail").css("color","red");
+	} else { zipTest(e); }
+	
+	/*carAddress1Test(e);
+	carAddress2Test(e);
+	carCityTest(e);
+	carZipTest(e);
+	memberIDTest(e); */
+	
+	if (homephone1 === '' && homephone2 === '' && homephone3 === '') {
+		e.preventDefault();
+	      $('#phonefail').show();
+	      $('#phonefail').text('Home/Cell Phone is required');
+	      $("#phonefail").css("color","red");
+	} else { phoneTest(e); }
+	
+/*	workPhoneTest(e);
+	
+	yearmakemodelTest(e);
+	mileageTest(e);
+	commentsTest(e);
+	otherTest(e);	
+	yearmakemodelTest(e);  */
+	//setTimeout(function(){ reloadCaptcha(); }, 100);
+});
+
+
+$('#registersButton').click(function(e) {
 	var fname = $.trim($('#First_Name').val());
 	var lname = $.trim($('#Last_Name').val());
 	var email = $.trim($('#Email').val());
