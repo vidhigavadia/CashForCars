d44b108f8372520de822a81356d69ddf97d31c23
vidhigavadia <vidhi.gavadia@gmail.com>
2016-03-14 17:36:48 -0500

    Update common2.js

--- js/common2.js
+++ js/common2.js
@@ -932,9 +932,7 @@ $('#registerButton').click(function(e) {
 	var fname = $.trim($('#First_Name').val());
 	var lname = $.trim($('#Last_Name').val());
 	var email = $.trim($('#Email').val());
-//	var addr1 = $.trim($('#Mailing_Address1').val());
-//	var city = $.trim($('#City').val());
-//	var state = $.trim($('#State :selected').text()); 
+
 	var zip = $.trim($('#Zip').val());
 	var homephone1 = $.trim($('#Home_Telephone_Ac').val());
 	var homephone2 = $.trim($('#homephonea').val());
@@ -957,33 +955,7 @@ $('#registerButton').click(function(e) {
 	} else { lnameTest(e); }
 
 	emailTest(e);
-	
-	// Address Line-1
-/*	if(addr1==='') {
-		e.preventDefault();
-  $('#adr1fail').show();
-    $('#adr1fail').text('Address Line one is required');
-    $("#adr1fail").css("color","red");
-	} else { address1Test(e); } 
-	
-	address2Test(e); */
-	
-	// City
-/*	if(city==='') {
-		e.preventDefault();
-    $('#cityfail').show();
-    $('#cityfail').text('City is required');
-    $("#cityfail").css("color","red");
-	} else { cityTest(e); }
-	
-	// State
-	if(state==='') {
-		e.preventDefault();
-    $('#statefail').show();
-    $('#statefail').text('State must be selected');
-    $("#statefail").css("color","red"); 
-	} */
-	
+
 	// Zipcode
 	if(zip==='') {
 		e.preventDefault();
@@ -992,11 +964,7 @@ $('#registerButton').click(function(e) {
     $("#zipfail").css("color","red");
 	} else { zipTest(e); }
 	
-/*	carAddress1Test(e);
-	carAddress2Test(e);
-	carCityTest(e);
-	carZipTest(e);
-	memberIDTest(e); */
+
 	
 	if (homephone1 === '' && homephone2 === '' && homephone3 === '') {
 		e.preventDefault();
@@ -1005,13 +973,7 @@ $('#registerButton').click(function(e) {
 	      $("#phonefail").css("color","red");
 	} else { phoneTest(e); }
 	
-//	workPhoneTest(e);
-	
-	yearmakemodelTest(e);
-//	mileageTest(e);
-//	commentsTest(e);
-	otherTest(e);	
-	yearmakemodelTest(e);  
+
 	//setTimeout(function(){ reloadCaptcha(); }, 100);
 });
 
